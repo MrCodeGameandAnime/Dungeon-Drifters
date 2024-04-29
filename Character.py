580636ecefe2bd7808ca17dcfabf29dbaefd21fe
Wesley Ruede <69434842+MrCodeGameandAnime@users.noreply.github.com>
2024-04-29 13:40:50 -0400

    Update

--- Character.py
+++ Character.py
@@ -3,60 +3,67 @@
 class CharacterType:
 
     class Brawler:
-
         def __init__(self):
             # strength -> attack, constituion -> defense
-            self.strength = 3
-            self.constitution = 2
-            self.health = 50
-            self. mana = 20
+            self.strength = 3 # raw power
+            self.constitution = 3 # defense and health
+            self.intelligence = 7 # magic
+            self.dexterity = 2 # finnese
+            self.charisma = 0 # skill check to talk your way through things or get better prices
+            self.health = 100
+            self.mana = 20
             self.exp = 0
             self.name = "Hero"
             self.level = 1
 
     class BlackMage:
-
         def __init__(self):
             # strength -> attack, constituion -> defense
+            # strength -> attack, constituion -> defense
             self.strength = 3
-            self.constitution = 2
+            self.constitution = 3
+            self.intelligence = 7
+            self.dexterity = 2
+            self.charisma = 0
             self.health = 50
-            self. mana = 20
+            self.mana = 20
             self.exp = 0
             self.name = "Hero"
             self.level = 1
 
     class RougeArcher:
-
         def __init__(self):
             # strength -> attack, constituion -> defense
             self.strength = 3
-            self.constitution = 2
+            self.constitution = 3
+            self.intelligence = 7
+            self.dexterity = 2
+            self.charisma = 0
             self.health = 50
-            self. mana = 20
+            self.mana = 20
             self.exp = 0
             self.name = "Hero"
             self.level = 1
 
     class Monk:
-
         def __init__(self):
             # strength -> attack, constituion -> defense
             self.strength = 3
-            self.constitution = 2
+            self.constitution = 3
+            self.intelligence = 7
+            self.dexterity = 2
+            self.charisma = 0
             self.health = 50
-            self. mana = 20
+            self.mana = 20
             self.exp = 0
             self.name = "Hero"
             self.level = 1
 
 class Level():
-
     def increaseLevel(self):
         pass
 
 class Health():
-
     def increaseHealth(self):
         pass
 
@@ -64,7 +71,6 @@ class Health():
         pass
         
 class Mana():
-
     def increaseMana(self):
         pass
     
@@ -75,7 +81,6 @@ class Mana():
         pass
 
 class Stats():
-
     def attack(self):
         pass
 
@@ -92,7 +97,6 @@ class Stats():
         pass
 
 class Exp():
-     
     def increaseExp(self):
         pass
 
--- Enemy.py
+++ Enemy.py
@@ -3,9 +3,72 @@
 class EnemyType:
 
     class Goblin:
-
         def __init__(self):
-            # strength -> attack, constituion -> defense
+            self.strength = 3
+            self.constitution = 2
+            self.health = 50
+            self.mana = 20
+            self.exp = 0
+            self.name = "Drak"
+            self.level = 1
+            
+        def printName(self):
+            print(self.name)
+
+    class Orcs:
+        def __init__(self):
+            self.strength = 3
+            self.constitution = 2
+            self.health = 50
+            self.mana = 20
+            self.exp = 0
+            self.name = "Drak"
+            self.level = 1
+            
+        def printName(self):
+            print(self.name)
+    
+    class Skelleton:
+        def __init__(self):
+            self.strength = 3
+            self.constitution = 2
+            self.health = 50
+            self.mana = 20
+            self.exp = 0
+            self.name = "Drak"
+            self.level = 1
+            
+        def printName(self):
+            print(self.name)
+    
+    class Archer:
+        def __init__(self):
+            self.strength = 3
+            self.constitution = 2
+            self.health = 50
+            self.mana = 20
+            self.exp = 0
+            self.name = "Drak"
+            self.level = 1
+            
+        def printName(self):
+            print(self.name)
+        
+    class Zombie:
+        def __init__(self):
+            self.strength = 3
+            self.constitution = 2
+            self.health = 50
+            self.mana = 20
+            self.exp = 0
+            self.name = "Drak"
+            self.level = 1
+            
+        def printName(self):
+            print(self.name)
+        
+    class SnakeLord:
+        def __init__(self):
             self.strength = 3
             self.constitution = 2
             self.health = 50
